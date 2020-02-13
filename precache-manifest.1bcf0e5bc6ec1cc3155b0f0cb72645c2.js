aeca77e34adfc4089be2c665eadd74585c108b95
Mary <marygans@microsoft.com>
2020-02-12 23:48:27 -0800

    Updates

---
+++ precache-manifest.1bcf0e5bc6ec1cc3155b0f0cb72645c2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa025ebff01a81791a49f2fca34c71d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a9268da3ddda92c666f",
+    "url": "/static/css/1.cfbbf99f.chunk.css"
+  },
+  {
+    "revision": "a20d62c079bfbb1fc065",
+    "url": "/static/css/4.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "56dac60a26392427f133",
+    "url": "/static/css/main.cf59633b.chunk.css"
+  },
+  {
+    "revision": "84b920731ba9f7b41811",
+    "url": "/static/js/0.d4d190c3.chunk.js"
+  },
+  {
+    "revision": "b698d4d237592cff204140000d841230",
+    "url": "/static/js/0.d4d190c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a9268da3ddda92c666f",
+    "url": "/static/js/1.0bfb134e.chunk.js"
+  },
+  {
+    "revision": "d80a967d65562e8ac46b11937d3ba69e",
+    "url": "/static/js/1.0bfb134e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ef5d7c19157d412796d",
+    "url": "/static/js/10.01781a1b.chunk.js"
+  },
+  {
+    "revision": "1c3ee3a80bb39808f098",
+    "url": "/static/js/11.a34b57fc.chunk.js"
+  },
+  {
+    "revision": "a20d62c079bfbb1fc065",
+    "url": "/static/js/4.efe53f2a.chunk.js"
+  },
+  {
+    "revision": "f007e5fde3f2c3983c1b0d874ba90212",
+    "url": "/static/js/4.efe53f2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f300820d24212c300c21",
+    "url": "/static/js/5.162940a0.chunk.js"
+  },
+  {
+    "revision": "d5869a428fb604ea4b95",
+    "url": "/static/js/6.90552833.chunk.js"
+  },
+  {
+    "revision": "5ee3bf1eaa6094732ce3",
+    "url": "/static/js/7.d9f632db.chunk.js"
+  },
+  {
+    "revision": "a03c5d3b853be40e6503",
+    "url": "/static/js/8.20fb6ef7.chunk.js"
+  },
+  {
+    "revision": "af4d6922f873b105354e",
+    "url": "/static/js/9.330dd45b.chunk.js"
+  },
+  {
+    "revision": "56dac60a26392427f133",
+    "url": "/static/js/main.ded74d9c.chunk.js"
+  },
+  {
+    "revision": "fb509afac9c5741e8e93",
+    "url": "/static/js/runtime-main.44224ca2.js"
+  }
+]);
